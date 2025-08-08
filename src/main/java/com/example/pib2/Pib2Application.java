b28fa4254138453ecdf2a17f97296b26c6d5a8e9
Santiago Correa <stillsantiago@gmail.com>
2025-08-08 18:25:54 -0500

    Se instala la dependencia swagger

--- src/main/java/com/example/pib2/Pib2Application.java
+++ src/main/java/com/example/pib2/Pib2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Pib2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Pib2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Pib2Application.class, args);
+    }
 
 }
